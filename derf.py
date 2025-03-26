4373991cd29cf8a129320942bf45a83aa1009af1
ajogb <apriloggg@gmail.com>
2025-03-25 21:08:12 -0400

    initial derf_int class

---
+++ derf.py
@@ -0,0 +1,40 @@
+import numpy as np
+DERF_SYMBOL = "߈̴"
+DERF_DIGITS = ("0","1","2","3","4","5",DERF_SYMBOL,"6","7","8","9")
+
+class derf_int():
+    def __init__(self,b10_int):
+        self.b10 = b10_int
+        self.b11 = np.base_repr(b10_int,11)
+        self.derf = b11_to_derf(self.b11)
+
+    def __str__(self):
+        return self.derf
+    
+    def __add__(self,other):
+        return derf_int(self.b10 + other.b10)
+    
+    def __sub__(self,other):
+        return derf_int(self.b10 - other.b10)
+    
+    def __mul__(self,other):
+        return derf_int(self.b10 * other.b10)
+
+def b10_to_derf(input: int):
+    return b11_to_derf(np.base_repr(input,11))
+
+def b11_to_derf(input: str):
+    output = ""
+    for i in range(len(input)):
+        if input[i] == "A":
+            output = output + DERF_DIGITS[10]
+        else: output = output + DERF_DIGITS[int(input[i])]
+    return output
+
+print(derf_int(21).b11)
+derf21 = derf_int(21)
+derf6 = derf_int(6)
+
+print(derf21 + derf6)
+print(derf21 - derf_int(4))
+print(derf21 * derf6)
