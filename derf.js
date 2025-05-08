9859ab9f2be584ce66c70def0399d676ca0eb474
ajogb <ajogb@gamer.com>
2025-05-08 11:53:48 -0400

    WIP conversion to class (there's definitely a better way to do this)

--- derf.js
+++ derf.js
@@ -1,54 +1,50 @@
-class DerfInt {
-    constructor(b10) {
-        this.b10Int = b10
-        this.b11Int = b10Tob11(this.b10Int)
-        this.derfInt = b11ToDerf(this.b11Int)
-    };
-};
-
-function b10Tob11(b10) {
+export class DerfInt {
+  constructor(b10) {
+    this.b10Int = b10;
+    this.b11Int = Derf.b10Tob11(this.b10Int);
+    this.derfInt = Derf.b11ToDerf(this.b11Int);
+  }
+}
+export class Derf {
+  constructor () {}
+  b10Tob11(b10) {
     let x = b10;
     let b = 11;
     let b11 = "";
     while (x > 0) {
-        b11 = B11_DIGITS[x % b] + b11;
-        x = Math.floor(x/b);
-    };
+      b11 = B11_DIGITS[x % b] + b11;
+      x = Math.floor(x / b);
+    }
     return b11;
-};
-
-function b11ToDerf(b11) {
+  }
+  b11ToDerf(b11) {
     let derf = "";
-    for (i=0; i<b11.length; i++) {
-        derf = derf + b11[i]
-    };
-    return derf
+    for (i = 0; i < b11.length; i++) {
+      derf = derf + b11[i];
+    }
+    return derf;
+  }
+  b10ToDerf(b10) {
+    return this.b11ToDerf(this.b10Tob11(b10));
+  }
+  derfAdd(op1, op2) {
+    return new DerfInt(op1.b10Int + op2.b10Int);
+  }
+  derfSub(op1, op2) {
+    return new DerfInt(op1.b10Int - op2.b10Int);
+  }
+  derfMul(op1, op2) {
+    return new DerfInt(op1.b10Int * op2.b10Int);
+  }
+  derfDiv(op1, op2) {
+    return new DerfInt(Math.floor(op1.b10Int / op2.b10Int));
+  }
+  derfMod(op1, op2) {
+    return new DerfInt(op1.b10Int % op2.b10Int);
+  }
+
+  static DERF_SYMBOL = "߈";
+  static B11_DIGITS = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a"];
+  static DERF_DIGITS = ["0", "1", "2", "3", "4", "5", this.DERF_SYMBOL, "6", "7", "8", "9",];
 };
 
-function b10ToDerf(b10) {
-    return b11ToDerf(b10Tob11);
-};
-
-function derfAdd(op1,op2) {
-    return DerfInt(op1.b10Int + op2.b10Int);
-};
-
-function derfSub(op1,op2) {
-    return DerfInt(op1.b10Int - op2.b10Int);
-};
-
-function derfMul(op1,op2) {
-    return DerfInt(op1.b10Int * op2.b10Int);
-};
-
-function derfDiv(op1,op2) {
-    return DerfInt(Math.floor(op1.b10Int / op2.b10Int))
-};
-
-function derfMod(op1,op2) {
-    return DerfInt(op1.b10Int % op2.b10Int);
-}
-
-const DERF_SYMBOL = "߈";
-const B11_DIGITS = ["0","1","2","3","4","5","6","7","8","9","a"];
-const DERF_DIGITS = ["0","1","2","3","4","5",DERF_SYMBOL,"6","7","8","9"];
