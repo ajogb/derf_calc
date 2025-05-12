db23d3aca02bef2f57edbfd0a69fb11de36a6ad8
ajogb <ajogb@gamer.com>
2025-05-12 13:43:36 -0400

    todo: separate numBar display and internal tracking of values

--- calculator.js
+++ calculator.js
@@ -2,6 +2,10 @@ const numbar = document.querySelector("#num-display");
 const numButtons = document.querySelectorAll(".numbutton");
 const opButtons = document.querySelectorAll('[class*=" op-"]');
 
+let num1 = "0";
+let num2 = "";
+let op = "";
+
 for (button of numButtons) {
   button.addEventListener("click", numClick);
 }
@@ -39,7 +43,9 @@ function execOp(opClass) {
     case "op-bs":
       if (numbar.value.length !== 1) numbar.value = numbar.value.slice(0, -1);
       else numbar.value = 0;
+      break;
     case "op-clr":
+      numbar.value = 0;
       break;
     default:
       break;
@@ -51,4 +57,8 @@ function addDigit(digit) {
     numbar.value = "";
   }
   numbar.value = `${numbar.value}${digit}`;
+}
+
+function numbarDisplay() {
+
 }
